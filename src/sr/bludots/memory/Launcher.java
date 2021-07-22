75b0a4f52880d3634292edd0ce0b77b8c9c71bf3
Henry Neijhorst <henry@MacBook-Air.fritz.box>
2021-07-22 21:34:19 +0200

    changes

--- src/sr/bludots/memory/Launcher.java
+++ src/sr/bludots/memory/Launcher.java
@@ -3,8 +3,32 @@ package sr.bludots.memory;
 public class Launcher {
 	
 	public static void main(String args[]) {
+		String a8 = "abcdefgh";
+		String a9 = "abcdefGh";
+		int nrTries = 0;
+		
+		
+		int a1= 1;
+		int a2 = 2;
+		
+		//a8 = a8 + "gh";
+		
+		System.err.println("A8=" + a8 + "   en A9=" + a9);
+		
+		String a9lower = a9.toLowerCase();
+		
+		if (a8.equalsIgnoreCase(a9)) {
+			System.err.println("gelijk");
+		}
+		else {
+			System.err.println("ongelijk " );
+		}
 		Memory mem = new Memory();
-		mem.runNumbers();
+		//mem.runNumbers();
+		
+		if (nrTries  > 10) {
+			//stop
+		}
 	}
 
 	
--- src/sr/bludots/memory/Memory.java
+++ src/sr/bludots/memory/Memory.java
@@ -3,36 +3,40 @@ package sr.bludots.memory;
 import java.util.HashSet;
 import java.util.Scanner;
 
-
-/** 
- * This is an example of a memory game.
- * The computer remembers which numbers you have entered.
- *
+/**
+ * This is an example of a memory game. The computer remembers which numbers you
+ * have entered.
+ * 
+ * Assignment #1 (Easy): 
+ * Make sure that the user cannot enter more than "10" valid, unique numbers, after that stop
+ * 
+ * Assignment #2 (Easy)::
+ * The user can exit by entering quit or exit or stop
+ * 
+ * Assignment #3 (Hard)
+ * Instead of entering numbers the user can enter unique words (String)
+ * 
+ * Assignment #4 (Hard)
+ * Before starting, the users selects if he wants to use numbers or words
  */
 public class Memory {
 
 	// the HashSet (a collection which only contains unique) elements
 	private HashSet<Integer> knownNumbers;
-	
+
 	// a counter to record how many times a user has entered a value
 	private int numberOfTries;
-	
-	
-	public static void main(String args[]) {
-		Memory mem = new Memory();
-		mem.runNumbers();
-	}
-	
-	//Constructor, to create a Memory object
-	public Memory () {
-		
+
+
+	// Constructor, to create a Memory object
+	public Memory() {
+
 		// initialize the knownNumbers variable
 		knownNumbers = new HashSet<Integer>();
-		
+
 		// initialize the counter variable
 		numberOfTries = 0;
 	}
-	
 
 	/**
 	 * Keeps asking for numbers until the user provides quit input
@@ -41,53 +45,56 @@ public class Memory {
 		boolean result;
 		do {
 			result = requestInput();
-		}
-		while (result);
+		} while (result);
 		System.out.println("You have tried " + numberOfTries + "  times.");
-		System.out.println("System remembered "  + knownNumbers.size() + " numbers");
+		System.out.println("System remembered " + knownNumbers.size() + " numbers");
 	}
-	
+
 	/**
-	 * Request input from the user and report back if it is a known number
-	 * if the number is not known it is added to the memory
+	 * Request input from the user and report back if it is a known number if the
+	 * number is not known it is added to the memory
 	 * 
 	 * @return false if the user has enter quit (indicating he wants to leave)
 	 */
 	private boolean requestInput() {
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please enter a number: ");
 		String providedText = input.next();
+
+		boolean result;
 		
+			
 		if (providedText.equals("quit")) {
 			// the user provided an empty text, so we exit with false
-			return false;
+			result = false;
 		}
-		
-		// increase the try counter
-		numberOfTries++;
-		
-		// now we check to see if the text reflect a number
-		// in case parsing fails an exception will be thrown
-		try {
-			int providedNumber = Integer.parseInt(providedText);
-			// since we are here, no exception has been thrown and there is a valid number
-			
-			// check if the 
-			if (knownNumbers.contains(providedNumber)) {
-				System.out.println("Old number, you already used this number");
+		else {
+			// increase the try counter
+			numberOfTries++;
+
+			// now we check to see if the text reflect a number
+			// in case parsing fails an exception will be thrown
+			try {
+				int providedNumber = Integer.parseInt(providedText);
+				// since we are here, no exception has been thrown and there is a valid number
+
+				// check if the
+				if (knownNumbers.contains(providedNumber)) {
+					System.out.println("Old number, you already used this number");
+				} else {
+					// new number, add it too the set
+					knownNumbers.add(providedNumber);
+					System.out.println("New number!, we are remembering this");
+				}
+			} catch (Exception e) {
+				// look for exceptions, in this case a non valid number was entered
+				System.err.println("You entered an invalid number");
 			}
-			else {
-				// new number, add it too the set
-				knownNumbers.add(providedNumber);
-				System.out.println("New number!, we are remembering this");
-			}
-			return true;
+			result = true;
 		}
-		catch (Exception e) {
-			// look for exceptions, in this case a non valid number was entered
-			System.err.println("You entered an invalid number");
-		}
-		return true;
+		input.close();
+
+		return result;
 	}
 }
